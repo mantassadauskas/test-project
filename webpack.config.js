8199b851609fb2941827a59e30f2cbac21255d74
msadauskas <mantas.sadauskas@nfq.lt>
2021-02-02 11:54:55 +0200

    add source maps and error

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,43 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    devtool: 'eval-source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, "build")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.s?css$/,
-                use: [ {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: { publicPath: "" }
-                }, "css-loader", "sass-loader" ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [ "babel-loader" ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource'
-            }
+module.exports = env => {
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, "build")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/i,
+                    loader: 'html-loader',
+                },
+                {
+                    test: /\.s?css$/,
+                    use: [ {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "" }
+                    }, "css-loader", "sass-loader" ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [ "babel-loader" ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    type: 'asset/resource'
+                }
+            ]
+        },
+        devtool: env.debug ? 'eval-source-map' : false,
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "src", "index.html")
+            }),
+            new MiniCssExtractPlugin()
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html")
-        }),
-        new MiniCssExtractPlugin()
-    ]
+    }
 }
