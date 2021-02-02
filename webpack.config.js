39318b7395b3420700a43614a495df5497db87b3
msadauskas <mantas.sadauskas@nfq.lt>
2021-02-02 10:58:34 +0200

    remove sass support, clean assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -14,7 +15,7 @@ module.exports = {
                 loader: 'html-loader',
             },
             {
-                test: /\.(s[ac]|c)ss$/,
+                test: /\.s?css$/,
                 use: [ {
                     loader: MiniCssExtractPlugin.loader,
                     options: { publicPath: "" }
@@ -26,12 +27,13 @@ module.exports = {
                 use: [ "babel-loader" ]
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif|svg)$/i,
                 type: 'asset/resource'
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
         }),
